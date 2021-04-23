7da3d9a4a22e04935c6c48b45a0e0af181b61930
John Adler <john.adler@protonmail.ch>
2021-04-23 14:15:13 -0400

    Add constants.

--- leopardff16.go
+++ leopardff16.go
@@ -4,17 +4,29 @@ package leopard
 // Datatypes and Constants
 
 // Finite field element type
-// TODO use this
 type ffe_t = uint16
 
 // Number of bits per element
-const kBits = 16
+const kBits uint32 = 16
 
 // Finite field order: Number of elements in the field
-const kOrder = 65536
+const kOrder uint32 = 65536
 
 // Modulus for field operations
-const kModulus = 65535
+const kModulus ffe_t = 65535
 
 // LFSR Polynomial that generates the field elements
-const kPolynomial = 0x1002D
+const kPolynomial uint32 = 0x1002D
+
+// Basis used for generating logarithm tables
+// Using the Cantor basis here enables us to avoid a lot of extra calculations
+// when applying the formal derivative in decoding.
+var kCantorBasis = [...]ffe_t{
+	0x0001, 0xACCA, 0x3C0E, 0x163E,
+	0xC582, 0xED2E, 0x914C, 0x4012,
+	0x6C98, 0x10D8, 0x6A72, 0xB900,
+	0xFDB8, 0xFB34, 0xFF38, 0x991E,
+}
+
+//------------------------------------------------------------------------------
+// Field Operations

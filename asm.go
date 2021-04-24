c267691f65647c4734959efe404fec29570ca8a6
John Adler <john.adler@protonmail.ch>
2021-04-23 21:43:52 -0400

    Asm for xor_mem.

--- asm.go
+++ asm.go
@@ -6,28 +6,56 @@ package main
 import (
 	. "github.com/mmcloughlin/avo/build"
 	. "github.com/mmcloughlin/avo/operand"
-	. "github.com/mmcloughlin/avo/reg"
+	// . "github.com/mmcloughlin/avo/reg"
 )
 
 func main() {
-	TEXT("xor_mem", NOSPLIT, "func(vx []byte, vy []byte, bytes uint64)")
+	////////////////////////////////////////////////////////////////////////////
+	// func xor_mem
+	////////////////////////////////////////////////////////////////////////////
+	const unroll = 4
+	TEXT("asm_xor_mem", NOSPLIT, "func(vx []byte, vy []byte, bytes uint64)")
 	x32 := Mem{Base: Load(Param("vx").Base(), GP64())}
 	y32 := Mem{Base: Load(Param("vy").Base(), GP64())}
 	bytes := Load(Param("bytes"), GP64())
 
-	x0 := YMM()
-	x1 := YMM()
-	x2 := YMM()
-	x3 := YMM()
-	for bytes >= 128 {
-		VPXOR(x0, x32, y32)
-		x32 += 4
-		y32 += 4
-		bytes -= 128
+	rx := YMM()
+	ry := YMM()
+
+	Label("L1")
+	Comment("for bytes >= 128")
+	CMPQ(bytes, Imm(128))
+	JLT(LabelRef("Z1"))
+
+	for i := 0; i < unroll; i++ {
+		VMOVDQU(rx, x32.Offset(i))
+		VMOVDQU(ry, y32.Offset(i))
+		VPXOR(rx, rx, ry)
+		VMOVDQU(x32.Offset(i), rx)
 	}
-	if bytes > 0 {
+	Comment("x32 += 4")
+	ADDQ(Imm(unroll), x32)
+	Comment("y32 += 4")
+	ADDQ(Imm(unroll), y32)
+	Comment("bytes -= 128")
+	SUBQ(Imm(unroll*32), bytes)
+
+	Label("Z1")
+	Comment("if bytes > 0")
+	CMPQ(bytes, Imm(0))
+	JLE(LabelRef("Z2"))
+	for i := 0; i < 2; i++ {
+		VMOVDQU(rx, x32.Offset(i))
+		VMOVDQU(ry, y32.Offset(i))
+		VPXOR(rx, rx, ry)
+		VMOVDQU(x32.Offset(i), rx)
 	}
+
+	Label("Z2")
 	RET()
 
+	////////////////////////////////////////////////////////////////////////////
+	// Generate asm
+	////////////////////////////////////////////////////////////////////////////
 	Generate()
 }
